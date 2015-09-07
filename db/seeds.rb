19f6475209df9373dca1e44636bb3072246cf210
Vitalii Senych <vitalii.senych@perfectial.com>
2015-09-07 17:46:33 +0300

    Add Pages Controller and refactor UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create(email: 'yevgen@gmail.com', password: '63XhafRvb')
