2bb514a91131fb9cd52e055e031b25b96516757e
Vitaliy Senych <vsenych@dataxu.com>
2016-05-25 10:25:14 +0300

    Added categories to projects and params for displaying them.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,19 @@ class HomeController < ApplicationController
   end
 
   def projects
-    @commercial_projects = Page.tagged_with('project commercial').includes(:page_images)
-    @privat_projects = Page.tagged_with('project privat')
-    @finished_projects = Page.tagged_with('project finished')
+    @projects = Page.project.order(created_at: :desc).includes(:page_images)
+
+    @projects = case params[:project_type]
+    when 'commercial'
+      @projects.tagged_with('project commercial')
+    when 'private'
+      @projects.tagged_with('project privat')
+    when 'finished'
+      @projects.tagged_with('project finished')
+    else
+      @projects
+    end
+
+    @projects
   end
 end
